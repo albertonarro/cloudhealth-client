098ba101edddc93531fcb1ce62bb9c33eff2b4bf
Alberto Narro <alberto.narro@hotmail.com>
2023-02-26 20:59:14 +0100

    build: update files to v0.2.0

---
+++ bump-version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION=$1
+SEMVER_REGEX="^[vV]?(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)(\\-[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?(\\+[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?$"
+
+if [ -z "$VERSION" ]
+then
+    VERSION=$(git flow release list | grep '*' | awk '{print $2}') 
+fi
+
+if [[ "$VERSION" =~ $SEMVER_REGEX ]]
+then
+    sed -i "s/version=.*/version='$VERSION',/g" setup.py
+    echo Files modified successfully, version bumped to $VERSION
+else
+    echo 'Tag must match the semver scheme X.Y.Z[-PRERELEASE][+BUILD]. See https://semver.org/'
+fi
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cloudhealth-client',
-    version='0.1',
+    version='0.2.0',
     author='Alberto Narro',
     author_email='alberto.narro@hotmail.com',
     url = 'https://github.com/albertonarro/cloudhealth-client',
