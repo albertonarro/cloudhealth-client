b16bd53677b8a3e3d25e789ea6e845de36988be6
Alberto Narro <alberto.narro@hotmail.com>
2023-02-28 15:25:07 +0100

    feat(organizations): Create organization.

--- cloudhealth/organization.py
+++ cloudhealth/organization.py
@@ -6,6 +6,7 @@ class OrganizationClient():
     def __init__(self, client):
         self.client = client
 
+
     def list(self, page=None, per_page=None, org_id=None):
         uri = '/v2/organizations'
         params = []
@@ -21,6 +22,7 @@ class OrganizationClient():
 
         return organizations
 
+
     def update(self, org_id, description):
         uri = '/v2/organizations/{org_id}'
         data = { "description": description }
@@ -31,9 +33,26 @@ class OrganizationClient():
 
         return organization
 
+
     def delete(self, org_id):
         uri = '/v2/organizations/{org_id}'
 
         response = self.client.query(uri, method='DELETE')
 
         return response
+
+
+    def create(self, name, description=None, parent_organization_id=None):
+        uri = '/v2/organizations'
+        data = { "name": name }
+
+        if description:
+            data['description'] = description
+        if parent_organization_id:
+            data['parent_organization_id'] = parent_organization_id
+
+        organization = self.client.query(
+            uri, method='POST', data=json.dumps(data)
+        )
+
+        return organization
