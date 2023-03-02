cc3da3150990c59e2d900d2ba44cf023f6aa8201
Alberto Narro <alberto.narro@hotmail.com>
2023-03-02 22:28:24 +0100

    feat(partner): Delete a partner customer tenant from the CloudHealth Platform.

--- cloudhealth/partner.py
+++ cloudhealth/partner.py
@@ -63,28 +63,27 @@ class PartnerClient():
         return customer
 
 
-    def update_customer(self, client_api_id, name=None, street1=None, street2=None,city=None, state=None, zipcode=None, country=None, classification=None,trial_expiration=None, billing_contact=None,partner_billing_configuration=False,partner_billing_configuration_folder=None, tags=None):
+    def update_customer(self, client_api_id, name=None, street1=None, street2=None, city=None, state=None, zipcode=None, country=None, classification=None, trial_expiration=None, billing_contact=None, partner_billing_configuration=False, partner_billing_configuration_folder=None, tags=None):
         uri = f'/v1/customers/{client_api_id}'
-
         data = {
             "partner_billing_configuration": {},
             "address": {}
         }
 
-		if name:
-			data['name'] = name
-		if street1:
-			data['street1'] = street1
-		if street2:
+        if name:
+            data['name'] = name
+        if street1:
+            data['street1'] = street1
+        if street2:
             data['street2'] = street2
-		if city:
-			data['city'] = city
-		if state:
-			data['state'] = state
-		if zipcode:
-			data['zipcode'] = zipcode
-		if country:
-			data['country'] = country
+        if city:
+            data['city'] = city
+        if state:
+            data['state'] = state
+        if zipcode:
+            data['zipcode'] = zipcode
+        if country:
+            data['country'] = country
         if classification:
             data['classification'] = classification
         if isinstance(trial_expiration, datetime):
@@ -100,12 +99,20 @@ class PartnerClient():
         if tags:
             data['tags'] = tags
 
-		if not data['address']:
-			del data['address']
-		if not data['partner_billing_configuration']:
-			del data['partner_billing_configuration']
+        if not data['address']:
+            del data['address']
+        if not data['partner_billing_configuration']:
+            del data['partner_billing_configuration']
 
 
-		customer = self.client.query(uri, method='PUT', data=data)
+        customer = self.client.query(uri, method='PUT', data=data)
 
-		return customer
+        return customer
+
+
+    def delete_customer(self, client_api_id):
+        uri = f'/v1/customers/{client_api_id}'
+        
+        response = self.client.query(uri, method='DELETE')
+
+        return response
