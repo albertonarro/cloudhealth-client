44ed35a82c93a2b11e48e409ca50e5767c78e137
Alberto Narro <alberto.narro@hotmail.com>
2023-03-02 22:12:13 +0100

    feat(partner): Add a partner customer tenant in the CloudHealth Platform.

--- cloudhealth/partner.py
+++ cloudhealth/partner.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from . import exceptions
 
 class PartnerClient():
@@ -13,6 +14,7 @@ class PartnerClient():
 
         return report
 
+
     def list_assets(self, client_api_id, name):
         uri = '/api/search.json'
         params = [
@@ -24,3 +26,38 @@ class PartnerClient():
         assets = self.client.query(uri, method='GET', params=params)
 
         return assets
+
+
+    def create_customer(self, name=None, street1=None, street2=None,city=None, state=None, zipcode=None, country=None, classification=None,trial_expiration=None, billing_contact=None,partner_billing_configuration=False,partner_billing_configuration_folder=None, tags=None):
+        uri = '/v1/customers'
+        data = {
+            "name": name,
+            "address": {
+                "street1": street1,
+                "street2": street2,
+                "city": city,
+                "state": state,
+                "zipcode": zipcode,
+                "country": country
+            },
+            "partner_billing_configuration": {}
+        }
+
+        if classification:
+            data['classification'] = classification
+        if isinstance(trial_expiration, datetime):
+            data['trial_expiration'] = trial_expiration.isoformat()
+        if isinstance(trial_expiration, str):
+            data['trial_expiration'] = trial_expiration
+        if billing_contact:
+            data['billing_contact'] = billing_contact
+        if partner_billing_configuration:
+            data['partner_billing_configuration']['enabled'] = partner_billing_configuration
+        if partner_billing_configuration_folder:
+            data['partner_billing_configuration']['folder'] = partner_billing_configuration_folder
+        if tags:
+            data['tags'] = tags
+
+        customer = self.client.query(uri, method='POST', data=data)
+
+        return customer
