a944fc6e710b2c6f0ab893d1919681fb45b89391
Alberto Narro <alberto.narro@hotmail.com>
2023-03-02 22:24:07 +0100

    feat(partner): Modify a partner customer tenant that already exists in the CloudHealth Platform.

--- cloudhealth/partner.py
+++ cloudhealth/partner.py
@@ -61,3 +61,51 @@ class PartnerClient():
         customer = self.client.query(uri, method='POST', data=data)
 
         return customer
+
+
+    def update_customer(self, client_api_id, name=None, street1=None, street2=None,city=None, state=None, zipcode=None, country=None, classification=None,trial_expiration=None, billing_contact=None,partner_billing_configuration=False,partner_billing_configuration_folder=None, tags=None):
+        uri = f'/v1/customers/{client_api_id}'
+
+        data = {
+            "partner_billing_configuration": {},
+            "address": {}
+        }
+
+		if name:
+			data['name'] = name
+		if street1:
+			data['street1'] = street1
+		if street2:
+            data['street2'] = street2
+		if city:
+			data['city'] = city
+		if state:
+			data['state'] = state
+		if zipcode:
+			data['zipcode'] = zipcode
+		if country:
+			data['country'] = country
+        if classification:
+            data['classification'] = classification
+        if isinstance(trial_expiration, datetime):
+            data['trial_expiration'] = trial_expiration.isoformat()
+        if isinstance(trial_expiration, str):
+            data['trial_expiration'] = trial_expiration
+        if billing_contact:
+            data['billing_contact'] = billing_contact
+        if partner_billing_configuration:
+            data['partner_billing_configuration']['enabled'] = partner_billing_configuration
+        if partner_billing_configuration_folder:
+            data['partner_billing_configuration']['folder'] = partner_billing_configuration_folder
+        if tags:
+            data['tags'] = tags
+
+		if not data['address']:
+			del data['address']
+		if not data['partner_billing_configuration']:
+			del data['partner_billing_configuration']
+
+
+		customer = self.client.query(uri, method='PUT', data=data)
+
+		return customer
